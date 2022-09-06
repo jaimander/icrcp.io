b512d671e692129347004cf023f5cc6574aeff0b
Jaime Rodriguez Gómez <jaimerodriguezgo@gmail.com>
2022-09-06 12:26:56 -0500

    upd

--- script.js
+++ script.js
@@ -24,5 +24,61 @@ const handleMouseClic = (event) => {
   counter = 0;
 };
 
+function handleStart(event) {
+  event.preventDefault();
+  var touches = event.changedTouches;
+
+  for (var i=0; i<touches.length; i++) {
+    ongoingTouches.push(touches[i]);
+    var color = colorForTouch(touches[i]);
+    ctx.fillStyle = color;
+    ctx.fillRect(touches[i].pageX-2, touches[i].pageY-2, 4, 4);
+  }
+}
+
+function handleMove(event) {
+  event.preventDefault();
+  var touches = event.changedTouches;
+
+  ctx.lineWidth = 4;
+
+  for (var i=0; i<touches.length; i++) {
+    var color = colorForTouch(touches[i]);
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+    ctx.lineTo(touches[i].pageX, touches[i].pageY);
+    ctx.closePath();
+    ctx.stroke();
+    ongoingTouches.splice(idx, 1, touches[i]);  // swap in the new touch record
+  }
+}
+
+function handleEnd(event) {
+  event.preventDefault();
+  var touches = event.changedTouches;
+
+  ctx.lineWidth = 4;
+
+  for (var i=0; i<touches.length; i++) {
+    var color = colorForTouch(touches[i]);
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(ongoingTouches[i].pageX, ongoingTouches[i].pageY);
+    ctx.lineTo(touches[i].pageX, touches[i].pageY);
+    ongoingTouches.splice(i, 1);  // remove it; we're done
+  }
+}
+
+
+
+
 canvas.addEventListener('mousemove', handleMouseMove);
 canvas.addEventListener('click', handleMouseClic);
+canvas.addEventListener('touchmove', handleMove);
+canvas.addEventListener("touchstart", handleStart, false);
+canvas.addEventListener("touchend", handleEnd, false);
